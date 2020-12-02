f60aca85d4fc286dfe76aafb8dbe2263dd1d5bfd
Zatykó Nóra <71665153+Zatyi94@users.noreply.github.com>
2020-12-02 16:06:47 +0100

    Add files via upload

---
+++ set_tipusok.py
@@ -0,0 +1,23 @@
+# a set úgy néz ki, mint a dict, csak nincsenek valuek
+# a set nem tartalmazhat duplikált elemet
+# egyáltalán nem lehet benne 1 elemből 2x ua
+my_set = {3, 5, 6}
+print(my_set)
+print(type(my_set))
+
+my_set.add(4)
+my_set.add(3)
+my_set.add(3)
+my_set.add(3)
+my_set.add(3)
+print(my_set)
+
+my_set = {'asd', 4, 10, True}
+print(my_set)
+my_set.add(True)
+print(my_set)
+
+# kiszedjük egy listából a duplikált elemeket:
+my_list = [1, 1, 3, 3, 3, 4, 5]
+my_uniq_list = list(set(my_list))
+print(my_uniq_list)
